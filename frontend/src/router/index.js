a2b7ef1b60bf8224c47488ed6ed4b0f15fa1d33b
Renssus <RensWinckens@gmail.com>
2024-12-16 12:00:29 +0100

    I hate my life

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import DashboardView from '../views/dashboardView.vue'
-import TeamView from '../views/TeamView.vue'
-import TournooienView from '../views/TournooienView.vue'
-import MatchView from '../views/MatchView.vue'
-
-
-
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
+import RegisterView from '../views/RegisterView.vue';
+import DashboardView from '../views/dashboardView.vue';
+import TeamView from '../views/TeamView.vue';
+import TournooienView from '../views/TournooienView.vue';
+import MatchView from '../views/MatchView.vue';
 
 const routes = [
   { path: '/', component: HomeView },
@@ -16,13 +13,24 @@ const routes = [
   { path: '/register', component: RegisterView },
   { path: '/dashboard', component: DashboardView },
   { path: '/tournooi', component: TournooienView },
-  { path: '/team', component: TeamView },
+  { path: '/team', component: TeamView, meta: { requiresAuth: true } },
   { path: '/match', component: MatchView },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (requiresAuth && !user) {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
